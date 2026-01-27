50efcb3185c188b899f1ce117ed2caf14446ef76
SiquaraDev <matheus.siquara99@gmail.com>
2026-01-26 21:21:55 -0300

    adiciona os arquivos `database.js` e `.env`

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+// Cria uma conexão com o DB e lança uma query, depois encerra a conexão com o DB e retorna a resposta do servidor
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
