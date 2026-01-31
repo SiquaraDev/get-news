8edb33f5b5813ce2d59acd3c3d579fbaddd80075
SiquaraDev <matheus.siquara99@gmail.com>
2026-01-31 14:53:53 -0300

    faz o `database.js` `client.connect( )` mais robusto contra erros

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -11,7 +11,7 @@ test("GET para /api/v1/status deveria retornar 200", async () => {
   const parseUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parseUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual("16.11");
   expect(responseBody.dependencies.database.max_connections).toEqual(100);
   expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
